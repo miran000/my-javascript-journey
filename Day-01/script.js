63b3bc641b9be4e2dcbfd17514498d2229ac372e
MD. MIRAN HOSSAIN <sunmonwater826@gmail.com>
2026-05-08 08:45:06 +0600

    Update script.js

--- Day-01/script.js
+++ Day-01/script.js
@@ -1,23 +1,3 @@
-let name = "Miran";
-let age = 25;
-let isStudent = true;
-let favoriteProgrammingLanguage = "JavaScript";
-
-console.log ("Name: " + name);
-console.log ("Age: " + age);
-console.log ("Is Student: " + isStudent);
-console.log ("Favorite Programming Language: " + favoriteProgrammingLanguage);
-
-// create an Object and Array
-
-let person = {
-    name: "Miran Hossain",
-    age: 26,
-    isStudent: false,
-    favoriteProgrammingLanguage: "JavaScript and Python"
-};
-console.log(person);
-
-
-let programmingLanguages = ["JavaScript", "Python", "Java", "C++"];
-console.log(programmingLanguages);
+console.log("Hello, tapaScript!");
+console.log("Welcome to 40 Days of JavaScript!");
+document.write("Check the browser console for a message!");
