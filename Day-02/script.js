cf6fabf42a1473e7b8be87d08a2e243a53016ebb
MD. MIRAN HOSSAIN <sunmonwater826@gmail.com>
2026-05-08 08:43:20 +0600

    Add script.js with person object and languages array

---
+++ Day-02/script.js
@@ -0,0 +1,23 @@
+let name = "Miran";
+let age = 25;
+let isStudent = true;
+let favoriteProgrammingLanguage = "JavaScript";
+
+console.log ("Name: " + name);
+console.log ("Age: " + age);
+console.log ("Is Student: " + isStudent);
+console.log ("Favorite Programming Language: " + favoriteProgrammingLanguage);
+
+// create an Object and Array
+
+let person = {
+    name: "Miran Hossain",
+    age: 26,
+    isStudent: false,
+    favoriteProgrammingLanguage: "JavaScript and Python"
+};
+console.log(person);
+
+
+let programmingLanguages = ["JavaScript", "Python", "Java", "C++"];
+console.log(programmingLanguages);
